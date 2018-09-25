81345f84f74243a64cac6aba614b24e9b539cffe
hoaiyh <maithuhuongxtb991999@gmail.com>
2018-09-25 14:41:48 +0700

    bai tap tuan 3

--- src/week2/task1/Task1.java
+++ src/week2/task1/Task1.java
@@ -2,13 +2,35 @@ package week2.task1;
 
 public class Task1 {
 
+    /*
+     * To change this license header, choose License Headers in Project Properties.
+     * To change this template file, choose Tools | Templates
+     * and open the template in the editor.
+     */
+
+
     public static int gcd(int a, int b) {
         // TODO: Tính ước chung lớn nhất của 2 số a, b
-        return 0;
+        int c = 0;
+        for(int i=1;i<=a || i<=b;i++)
+        {
+            if(a%i == 0 && b%i==0)
+                c=i;
+        }
+        return c;
     }
 
     public static int fibonacci(int n) {
         // TODO: Tìm số fibonacci ở vị trí n
-        return 0;
+        if(n<0)
+            return -1;
+        else if(n==0||n==1)
+        {
+            return n;
+        }
+        else
+        {
+            return fibonacci(n-1) + fibonacci(n-2);
+        }
     }
 }
